66ffffccac0aa03a5d360effb282d8c732054a18
otavioaraujo08 <otavioaraujo490@gmail.com>
2021-10-04 11:37:46 -0300

    NavBar, Banner e Footer criados

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <h1>Footer</h1>
+    <footer id="footer">
+        <p>Monte seu sorvete &copy; 2021</p>
+    </footer>
 </template>
 
 <script>
@@ -9,5 +11,14 @@
 </script>
 
 <style>
-
+    #footer {
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="home">
-
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu Sorvete:</h1>
+    <p>Formulário</p>
   </div>
 </template>
 
 <script>
+  import Banner from "../components/Banner.vue"
 
+  export default {
+    name: "Home",
+    components: {
+      Banner
+    }
+  }
 </script>
